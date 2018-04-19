9b6f8d863ea510312ab5b7a2d91355c8605fceaf
Loelia Thevenin <loelia.thevenin@epitech.eu>
2018-04-19 17:20:00 +0200

    improved error handling

--- generator/error_handling.c
+++ generator/error_handling.c
@@ -7,11 +7,24 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include "macro.h"
+
+bool my_str_is_num(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0') {
+		if (!IS_NUM(str[i]))
+			return false;
+		++i;
+	}
+	return true;
+}
 
 bool error_handling(int ac, char **av)
 {
 	if (ac != 3)
 		return false;
-	printf("av[1] = %s\nav[2] = %s\n",av[1], av[2]);
+
 	return true;
 }
---
+++ generator/include/macro.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** DanteStar
+** File description:
+** macro.h
+*/
+
+#ifndef DANTESTAR_MACRO_H
+#define DANTESTAR_MACRO_H
+
+#define		IS_BETWEEN(a, b, c)     (c >= a && c <= b)
+#define		IS_ALPHA(c)     (IS_BETWEEN('a', 'z', c) && IS_BETWEEN('A', 'Z', c))
+#define		IS_NUM(c)       (IS_BETWEEN('0', '9', c))
+#define		IS_ALPHANUM(c)  (IS_ALPHA(c) && IS_NUM(c))
+
+#define		ERR -1
+#define		 OK 0
+#define		NULL_PTR ": String pointer is NULL"
+
+#endif //DANTESTAR_MACRO_H
