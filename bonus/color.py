a1c2431860812dffe746c1428cff21240e880436
Loelia Thevenin <loelia.thevenin@epitech.eu>
2018-05-11 14:58:28 +0200

    Now featuring: Blinking exit path

--- bonus/color.py
+++ bonus/color.py
@@ -1,4 +1,4 @@
 #!/bin/python3
 while True:
-    try:print(input().replace('X', '\x1B[91;49mX\x1B[0m').replace('o', '\x1B[36;49mo\x1B[0m').replace('*', '\x1B[31;49m*\x1B[0m'))
+    try:print(input().replace('X', '\x1B[91;49mX\x1B[0m').replace('o', '\x1B[5m\x1B[1m\x1B[36;49mo\x1B[0m').replace('*', '\x1B[31;49m*\x1B[0m'))
     except:break
